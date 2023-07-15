d04ae490299e8e6e52f6c465a04818db8d1e25b4
codyreading <codyreading@gmail.com>
2023-07-15 11:28:43 -0400

    Changed names of launch scripts

---
+++ launch_scripts/docker.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Command line arguments
+
+DETACH='False'
+Help()
+{
+  # Display help
+  echo "Usage: $0"
+  echo "[--interactive]"
+  echo "[--detach]"
+  echo ""
+  echo "Options:"
+  echo "--detach                       Run in detached mode (background)"
+  echo
+}
+
+while :; do
+  case $1 in
+  -h|-\?|--help)
+    Help   # Display a usage synopsis.
+    exit 1
+    ;;
+  -d|--detach)
+    DETACH='True'
+    ;;
+  ?*)
+    printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+    ;;
+  *)               # Default case: No more options, so break out of the loop.
+    break
+  esac
+
+  shift
+done
+
+set -e
+
+# Determine project root dir programmatically.
+SRC_PATH=$PWD
+DOCKER_IMAGE=nerfbusters
+
+
+CMD="docker run \
+  --rm \
+  --gpus all \
+  --runtime=nvidia \
+  -e NVIDIA_DRIVER_CAPABILITIES=compute,utility \
+  -e NVIDIA_VISIBLE_DEVICES=all \
+  -e TF_CPP_MIN_LOG_LEVEL=2 \
+  --net=host \
+  -u 0 \
+"
+
+
+if [[ $DETACH == 'False' ]]; then
+  CMD+="-it --entrypoint bash ${DOCKER_IMAGE}"
+else
+  CMD+="--detach ${DOCKER_IMAGE} bash scripts/run_baseline.sh"
+fi
+
+eval $CMD
+
+exit 0
