d00896fe0c6626c3b8db861687446ad58f066141
Cody Reading <codyreading@gmail.com>
2023-07-15 15:09:57 -0400

    Added psuedo gt

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -1,3 +1,4 @@
 python scripts/launch_nerf.py train --experiment-name baselines
+python scripts/launch_nerf.py train --experiment-name pseudo-gt
 bash scripts/render_all.sh
 bash scripts/metrics_all.sh
